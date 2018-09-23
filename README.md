2cf8964f29922c6b1c72fc13a9078a3b7dacbcaf
abedurftig <dasnervtdoch@googlemail.com>
2018-09-23 22:33:41 +0200

    Guide through config

--- README.md
+++ README.md
@@ -2,19 +2,25 @@
 
 Create and push a new Spring Boot project to GitHub.
 
+- Use the `spring-io/initializr` to create the Spring Boot project
+- Use the GitHub REST Api to create a remote repository on GitHub
+- Initialize a local Git repository and push it to the new GitHub repository
+
 ### Setup
 
 - Create a `Personal API Token` in your GitHub account (https://github.com/settings/tokens/new)
-- Define an env variable `GITHUB_API_TOKEN`
-- Define an env variable `GITHUB_USER`
-- Define an env variable `GITHUB_NAME`
+- Define an env variable `GITHUB_API_TOKEN` and set it to the `Personal API Token`
+- Define an env variable `GITHUB_USER` and set it to your GitHub username
 
 ### Run it
 
 ```
 https://raw.githubusercontent.com/abedurftig/spring-boot-on-github/master/create_new.sh | sh
 ```
+The script will guide through some basic configuration for the generated Spring Boot project.
 
 ### Resources
 
 - https://github.com/spring-io/initializr
+- https://developer.github.com/v3/
+- https://github.com/settings/tokens/new
--- create_new.sh
+++ create_new.sh
@@ -1,28 +1,101 @@
-echo "Call start.spring.io"
-curl https://start.spring.io/starter.zip -o $GITHUB_NAME.zip
-unzip -d $GITHUB_NAME $GITHUB_NAME.zip
-cd ./$GITHUB_NAME
+#!/bin/bash
 
-GITHUB_REQUEST_DATA=$(echo '{"name":"$GITHUB_NAME", "gitignore_template":"Gradle"}' | envsubst)
-echo "POST request data"
-echo $GITHUB_REQUEST_DATA
+if [ "$GITHUB_USER" = "" ] 
+then
+    echo "You have to set the GITHUB_USER env var"
+    exit 0
+fi
 
-echo "POST request"
+if [ "$GITHUB_API_TOKEN" = "" ] 
+then
+    echo "You have to set the GITHUB_API_TOKEN env var"
+    echo "Refer to https://github.com/settings/tokens/new"
+    exit 0
+fi
+
+echo "-------------------"
+echo "Setup a new project"
+echo "-------------------"
+echo "For more details refer to the spring-io/initializr docs: https://github.com/spring-io/initializr#generating-a-project"
+echo "-------------------"
+
+read -e -p "Project name [default: spring-boot-starter]: " PARAM_PROJECT_NAME
+[ -z "${PARAM_PROJECT_NAME}" ] && PARAM_PROJECT_NAME="spring-boot-starter"
+
+read -e -p "Comma separated list of dependencies [default: web,actuator]: " PARAM_DEPENDECIES
+[ -z "${PARAM_DEPENDECIES}" ] && PARAM_DEPENDECIES="web,actuator"
+
+read -e -p "GroupId [default: org.example]: " PARAM_GROUP_ID
+[ -z "${PARAM_GROUP_ID}" ] && PARAM_GROUP_ID="org.example"
+
+read -e -p "Description [default: ]: " PARAM_DESCRIPTION
+[ -z "${PARAM_DESCRIPTION}" ] && PARAM_DESCRIPTION=""
+
+read -e -p "Root package [default: $PARAM_GROUP_ID]: " PARAM_DESCRIPTION
+[ -z "${PARAM_ROOT}" ] && PARAM_ROOT=$PARAM_GROUP_ID
+
+read -e -p "Java version [default: 1.8]: " PARAM_JAVA
+[ -z "${PARAM_JAVA}" ] && PARAM_JAVA="1.8"
+
+read -e -p "Spring Boot version [default: 2.0.6.RELEASE]: " PARAM_BOOT
+[ -z "${PARAM_BOOT}" ] && PARAM_BOOT="2.0.6.RELEASE"
+
+echo "-------------"
+echo "Review config"
+echo "-------------"
+echo "-d name=$PARAM_PROJECT_NAME"
+echo "-d dependencies=$PARAM_DEPENDECIES"
+echo "-d groupId=$PARAM_GROUP_ID"
+echo "-d description=$PARAM_DESCRIPTION"
+echo "-d packageName=$PARAM_ROOT"
+echo "-d javaVersion=$PARAM_JAVA"
+echo "-d bootVersion=$PARAM_BOOT"
+
+read -e -p "Are you happy? [Y/n] " YN
+
+if [ "$YN" = "y" ] || [ "$YN" = "Y" ] || [ "$YN" = "" ]
+then
+    echo "All right. Let's continue..."
+else
+    echo "Just start over then."
+    exit 0
+fi
+
+echo "Get starter files from spring.io"
+mkdir $PARAM_PROJECT_NAME
+cd ./$PARAM_PROJECT_NAME
+
+curl https://start.spring.io/starter.tgz \
+-d language=java \
+-d type=gradle-project \
+-d name=$PARAM_PROJECT_NAME \
+-d dependencies=$PARAM_DEPENDECIES \
+-d groupId=$PARAM_GROUP_ID \
+-d description=$PARAM_DESCRIPTION \
+-d packageName=$PARAM_ROOT \
+-d javaVersion=$PARAM_JAVA \
+-d bootVersion=$PARAM_BOOT | tar -xzvf - &> /dev/null
+
+# echo "Create README"
+echo "This project was created with https://github.com/abedurftig/spring-boot-on-github." > README.md
+
+GITHUB_REQUEST_DATA=$(echo '{"name":"$PARAM_PROJECT_NAME", "description":"$PARAM_DESCRIPTION", "gitignore_template":"Gradle"}' | PARAM_PROJECT_NAME=$PARAM_PROJECT_NAME PARAM_DESCRIPTION=$PARAM_DESCRIPTION envsubst)
+
+echo "Create the remote GitHub repository with name $PARAM_PROJECT_NAME"
 curl \
 -u $GITHUB_USER:$GITHUB_API_TOKEN \
 --header "Content-Type: application/json" \
 --request POST \
 --data $GITHUB_REQUEST_DATA \
-https://api.github.com/user/repos
+https://api.github.com/user/repos &> /dev/null
 
-echo "Local git repo"
+echo "Init local Git repository"
 git init
 git add .
 git commit -m "Initial commit"
-git remote add origin https://github.com/$GITHUB_USER/$GITHUB_NAME.git
+echo "Setting remote to 'https://github.com/$GITHUB_USER/$PARAM_PROJECT_NAME.git'"
+git remote add origin https://github.com/$GITHUB_USER/$PARAM_PROJECT_NAME.git
+echo "Pushing to the GitHub repository"
 git push -u origin master
-
-echo "Cleaning up"
-rm ../$GITHUB_NAME.zip
 
 echo "Done."
